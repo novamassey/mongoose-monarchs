c06851ee8560113895c6b92902b754837c8d8b2f
Lauren N Massey <laurennmassey@Laurens-MacBook-Pro.local>
2021-12-15 16:03:10 -0800

    method-override installs requires mounts

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+require('dotenv').config();
+require('./config/database');
 
 var app = express();
 
@@ -17,6 +22,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
